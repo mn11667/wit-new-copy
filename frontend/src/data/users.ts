e27c86d0168c763c30e7b40f5af033cd84cba8e5
Madhav Neupane <mn11github@gmail.com>
2026-01-19 21:00:18 +0545

    rupesh added

--- frontend/src/data/users.ts
+++ frontend/src/data/users.ts
@@ -34,11 +34,9 @@ export const SEEDED_USERS = [
         password: import.meta.env.VITE_USER_AAKASH_PASSWORD,
         name: import.meta.env.VITE_USER_AAKASH_NAME
     },
-    // { email: 'student4@wit.com', password: 'wit_user_04', name: 'Student Four' },
-    // { email: 'student5@wit.com', password: 'wit_user_05', name: 'Student Five' },
-    // { email: 'student6@wit.com', password: 'wit_user_06', name: 'Student Six' },
-    // { email: 'student7@wit.com', password: 'wit_user_07', name: 'Student Seven' },
-    // { email: 'student8@wit.com', password: 'wit_user_08', name: 'Student Eight' },
-    // { email: 'student9@wit.com', password: 'wit_user_09', name: 'Student Nine' },
-    // { email: 'student10@wit.com', password: 'wit_user_10', name: 'Student Ten' }
+    {
+        email: import.meta.env.VITE_USER_RUPESH_EMAIL,
+        password: import.meta.env.VITE_USER_RUPESH_PASSWORD,
+        name: import.meta.env.VITE_USER_RUPESH_NAME
+    }
 ];
